c9124aee37a852ed360b02c3953bd2e90da64990
Xiaochaoy <xiaochaoy@elpuig.xeill.net>
2021-03-25 14:18:28 +0100

    version 2.1

--- src/com/company/PantallaBuscar.java
+++ src/com/company/PantallaBuscar.java
@@ -3,14 +3,15 @@ package com.company;
 public class PantallaBuscar {
     void mostrar() {
         Cabeza cabeza = new Cabeza();
-        cabeza.mostrar("Buscame esta .|.");
+        cabeza.mostrar("Buscame esta _.|..");
         System.out.print("A quien quieres buscar: ");
 
         int p = Main.scanner.nextInt();
         for (int i = 0; i < Main.personas.persona.size(); i++) {
             for (Persona persona : Main.personas.persona) {
                 if (p == i) {
-                    System.out.println(Main.personas.persona.get(p-1));
+                    System.out.println(persona.nom);
+                    System.out.println(persona.edat);
                 }
             }
         }
